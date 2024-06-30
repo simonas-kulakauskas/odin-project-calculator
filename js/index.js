4c79e15ed36992c85196350abc3ed22df0818865
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-30 01:25:50 +0100

    Added a some of the button even listeners and some functions for them

--- js/index.js
+++ js/index.js
@@ -18,3 +18,76 @@ const divide = (firstNumber, secondNumber) => {
 const modulus = (firstNumber, secondNumber) => {
     return firstNumber % secondNumber
 }
+
+let firstNumber, secondNumber, operator, display, displayValue;
+
+const operate = (operator, firstNumber, secondNumber) => {
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+        case "%":
+            return modulus(firstNumber, secondNumber);
+    }
+}
+
+display = document.getElementById("displayText");
+
+displayValue = 0;
+
+const populateDisplay = (value) => {
+    display.textContent = value;
+}
+populateDisplay(displayValue)
+
+const clearDisplay = () => {
+    display.textContent = "0"
+    displayValue = 0;
+}
+
+const saveNumber = (value) => {
+    value = value.target.value;
+    if (firstNumber === undefined) {
+        firstNumber = value;
+    } else if (secondNumber === undefined) {
+        secondNumber = value;
+    }
+}
+
+
+
+const numberButtons = Array.from(document.querySelectorAll("button")).filter((button) => {
+    if (button.value != "") {
+        return button;
+    }
+})
+
+for (let i = 0; i < numberButtons.length; i++) { // Attach number buttons listeners
+    numberButtons[i].addEventListener("click", saveNumber)
+}
+
+// Clear button
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearDisplay);
+
+// Operator buttons
+const addButton = document.getElementById("addition").addEventListener("click", () => {
+    operator = "+"
+})
+const subtractButton = document.getElementById("subtract").addEventListener("click", () => {
+    operator = "-"
+})
+const divideButton = document.getElementById("divide").addEventListener("click", () => {
+    operator = "/"
+})
+const multiplyButton = document.getElementById("multiply").addEventListener("click", () => {
+    operator = "*"
+})
+const modulusButton = document.getElementById("modulus").addEventListener("click", () => {
+    operator = "%"
+})
